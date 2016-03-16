7c15d131d2e6eb215a0e0f1ce188981ef110dc49
Christian Trosclair <xn@users.noreply.github.com>
2016-03-16 16:17:39 -0500

    Add missing api functions

--- addon/services/intercom.js
+++ addon/services/intercom.js
@@ -7,5 +7,46 @@ export default Ember.Service.extend({
 
   update(params) {
     Intercom('update', params);
+  },
+  
+  shutdown() {
+    Intercom('shutdown');
+  },
+  
+  show() {
+    Intercom('show');
+  },
+  
+  hide() {
+    Intercom('hide');
+  },
+  
+  shutdown() {
+    Intercom('shutdown');
+  },
+  
+  showMessages() {
+    Intercom('showMessages');
+  },
+  
+  showNewMessage(params) {
+    Intercom('showNewMessage', params);
+  },
+  
+  onHide(params) {
+    Intercom('onHide', params);
+  },
+  
+  onShow(params) {
+    Intercom('onShow', params);
+  },
+  
+  onActivatorClick(params) {
+    Intercom('onActivatorClick', params);
+  },
+  
+  trackEvent(eventName, metadata) {
+    Intercom('trackEvent', eventName, metadata);
   }
+  
 });
